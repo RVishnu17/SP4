6368908d68969d81aa8e459810f7bb23bce8e7ae
witsvishhanu <vrajakumaran@nuwa.ie>
2026-04-05 22:46:21 +0530

    3

--- sec1/src/main/java/learning/init/Rest/SayHello.java
+++ sec1/src/main/java/learning/init/Rest/SayHello.java
@@ -1,15 +1,19 @@
 package learning.init.Rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SayHello {
-
+    @Value("${swathi.name}")
+    public String swathi;
+    @Value("${vishnu.name}")
+    public String vishnu;
     @GetMapping("/")
     public String SayHello()
     {
-        return "Hello Swathi!";
+        return ("Hello !" + swathi + "Kannukkutty loves " +  vishnu + " !  " );
     }
 
     @GetMapping("/bye")
